7870252a5e29a429f527df54cde8eec4f425bd5c
Neel <84484122+neel-banga@users.noreply.github.com>
2023-02-05 03:10:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request
 import os
 import face_model
-import cv2
 import voice_model
 
-val = None
-val2 = None
 app = Flask(__name__)
 
 
@@ -20,6 +17,8 @@ def upload_photo():
   photo.save(os.path.join(os.getcwd(), photo.filename))
 
   val = face_model.check_intoxicated(photo.filename)
+  with open('val.txt', 'w') as f:
+    f.write(str(val))
   print(val)
 
   return render_template('voice_model.html')
@@ -27,26 +26,28 @@ def upload_photo():
 @app.route('/upload_video', methods=['POST'])
 def upload_video():
   video = request.files['video']
-  video_filename = os.path.join(os.getcwd(), video.filename)
-  video.save(video_filename)
+  video.save(video.filename)
 
   phrase = voice_model.get_phrase(2)
   print(phrase)
+
   with open('phrase.txt', 'w') as f:
     f.write(phrase)
 
   voice_model.get_wav_file(video.filename)
 
-  val2 = voice_model.check_slurring('test.wav', phrase)
+  val25, val2 = voice_model.check_slurring('test.wav', phrase)
+  print(val2)
+
+  r = open('val.txt', 'r')
+  val = r.read()
+  val = int(val)
+
+  print(val+val2)
 
   if ((val + val2)/2) >= 0.6:
     return render_template('not_intoxicated.html')
   else:
     return render_template('intoxicated.html')
-
-@app.route('/ph')
-def ph():
-  print('hi')
-  return render_template('camera.html')
 
 app.run(host='0.0.0.0', port=18)
