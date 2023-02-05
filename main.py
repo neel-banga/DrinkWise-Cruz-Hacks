c88aae400397f4fc8fd39dd744a42ad41b338da1
Neel <84484122+neel-banga@users.noreply.github.com>
2023-02-04 23:42:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 import os
 import face_model
+import voice_model
 
 
 val = None
+val2 = None
 app = Flask(__name__)
 
 
 @app.route('/')
 def camera():
-  return render_template("camera.html")
+  return render_template('camera.html')
 
 
 @app.route('/upload_photo', methods=['POST'])
@@ -21,6 +23,19 @@ def upload_photo():
   print(val)
 
   return render_template('voice_model.html')
+
+@app.route('/upload_video', methods=['POST'])
+def upload_video():
+  video = request.files['video']
+  video.save(os.path.join(os.getcwd(), video.filename))
+
+  val2 = voice_model.check_slurring(video.filename)
+
+  if ((val + val2)/2) >= 0.6:
+    return render_template('not_intoxicated.html')
+  else:
+    return render_template('intoxicated.html')
+
 
   '''
   if val == 0:
